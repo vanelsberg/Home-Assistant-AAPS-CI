c1f1b28f89d8303e9290e13337df8bef3fdc737d
vanelsberg <vanelsberg@outlook.com>
2025-08-19 21:03:29 +0200

    Replace GET by curl command

--- AAPS-CI/status.sh
+++ AAPS-CI/status.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Set current dit to the location of this script
-cd .
+cd "$(dirname "$(readlink -f "$0")")"
 
+# Set configuration
 source ./config.sh
 
 echo -e "Workflow status for $OWNER/$REPO, ID=$WORKFLOW_ID:\n"
 
 # Get json response and order by created_at (most recent item las)
 #
-GET https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows/${WORKFLOW_ID}/runs | \
+curl https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows/${WORKFLOW_ID}/runs | \
 jq '.workflow_runs | sort_by(.created_at) | map(select(.name == "AAPS CI")) | map({display_title, status, conclusion, run_number, created_at, html_url})'
 
