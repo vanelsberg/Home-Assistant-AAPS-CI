a71bf25d4a7b944efb8458e270a998241bcae149
vanelsberg <vanelsberg@outlook.com>
2026-05-04 22:37:23 +0200

    Use unauthenticated API request for starters (may hit GitHub's max request rate)

--- AAPS-CI/ci_workflows.sh
+++ AAPS-CI/ci_workflows.sh
@@ -5,8 +5,13 @@ source ./ci.config
 echo -e "Getting active workflows for repo $OWNER/$REPO:\n"
 
 res=$(curl -H "Accept: application/vnd.github.v3+json" \
-     -H "Authorization: Bearer $WF_PERSONAL_ACCESS_TOKEN" \
      https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows)
 
-#echo $res | jq
+# When you are hitting GitHuv request rating the above may not work whan used multiple times
+# To solve, uncomment below to use authenticated requests (and make sure to define WF_PERSONAL_ACCESS_TOKEN):
+
+# res=$(curl -H "Accept: application/vnd.github.v3+json" \
+#     -H "Authorization: Bearer $WF_PERSONAL_ACCESS_TOKEN" \
+#     https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows)
+
 echo $res | jq '.workflows[] | {id, name, url}'
